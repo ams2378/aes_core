22ffbd2795ae89d9195cb081680bfd4b4cd21965
Adil Sadik <ams2378@columbia.edu>
2013-02-22 02:50:34 -0500

    edit

--- bench/test.sv
+++ bench/test.sv
@@ -1,33 +1,5 @@
 `timescale 1ns/1ps
 
-class aes_transaction;
-	rand int 	unsigned text[4];
-	rand int 	unsigned key[4];
-	rand bit 	rst;
-	rand bit	ld;
-	bit		done;
-	int		status;
-
-	int 		ld_density;
-	int		rst_density;
-
-	function new (int ld_den, int rst_den);
-		ld_density = ld_den;
-		rst_density = rst_den;
-	endfunction
-
-	constraint density_dist {
-		ld dist {0:/100-ld_density, 1:/ld_density};
-		rst dist {0:/100-rst_density, 1:/rst_density};
-	}
-
-	constraint ld_status {
-		(status != 0) -> (ld == 0);
-	}
-
-endclass
-
-
 program tb (ifc.bench ds);
 
 	import "DPI-C" function void rebuild_text ( input int  txt, input int i);

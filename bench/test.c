f4f512b0360fc65b5db40d43639bdbbdd1c49fc9
Adil Sadik <ams2378@columbia.edu>
2013-02-22 02:18:30 -0500

    edit

--- bench/test.c
+++ bench/test.c
@@ -63,11 +63,9 @@ void send_ld_rst (int l, int rt) {
 	ld = l;
 
 //	if (status == 13 || rst == 0)
-	if (rst == 0 && status == 13){
-		done = 1;
+	if (rst == 0 && status != 13 ){
 		status = 0;
-	} else if (rst == 0)
-		status = 0;
+	} 
 
 	if (status >= 1)
 		status = status + 1;

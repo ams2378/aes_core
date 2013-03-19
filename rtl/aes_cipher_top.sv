a6317c76ee84d216c1f25dcd3df556730b3e0cc8
Adil Sadik <ams2378@columbia.edu>
2013-03-19 15:57:16 -0400

    edit

--- rtl/aes_cipher_top.sv
+++ rtl/aes_cipher_top.sv
@@ -72,6 +72,7 @@ module aes_cipher_top(clk, rst, ld, done, key, text_in, text_out,
 input		clk, rst;
 input		ld;
 output		done;
+output		ld_r;
 input	[127:0]	key;
 input	[127:0]	text_in;
 output	[127:0]	text_out;

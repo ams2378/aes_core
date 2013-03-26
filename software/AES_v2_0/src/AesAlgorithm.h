61e458b3c0601f2ab6e69992b641375a0db9867d
Adil Sadik <ams2378@columbia.edu>
2013-03-25 22:18:21 -0400

    edit

--- software/AES_v2_0/src/AesAlgorithm.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AES_ALGORITHM_
-#define _AES_ALGORITHM_
-
-
-
-extern void  aes_encrypt( uint32 * pInput , uint32 * pOutput, uint32 * pKey );
-
-#endif // _AES_ALGORITHM_
--- software/beecrypt-4.2.1/include/beecrypt/c++/posix.h
+++
@@ -1,9 +0,0 @@
-static void posixErrorDetector(const int err, const char* message) throw (Error)
-{
-        if (err)
-        {
-		errno = err;
-                perror(message);
-                throw Error(String(message) + String(": ") + String(strerror(err)));
-        }
-}

a410c33b8fca88b269b5d0ad5c080507d77fe190
Adil Sadik <ams2378@columbia.edu>
2013-03-27 00:45:56 -0400

    edit

--- bench/test.sv
+++ bench/test.sv
@@ -157,7 +157,12 @@ program tb (ifc.bench ds);
 //		$fdisplay (f,"rst : %b", t.rst );
 //		$fdisplay (f,"Key load : %b ", t.ld);
 
-
+		if (start == 1) begin
+		$fdisplay (g,"Encryption Number : %0d" , en_num);
+		$fdisplay (g,"KEY: %h%h%h%h", t.key[127:96], t.key[95:64], t.key[63:32], t.key[31:0]);
+		$fdisplay (g,"TEXT: %h%h%h%h", t.text[3], t.text[2], t.text[1], t.text[0]);
+		start = 0;
+		end
 		
 //		$fdisplay (f,"Inputs to roundkey : ");
 
@@ -268,15 +273,12 @@ program tb (ifc.bench ds);
 		$fdisplay (f,"Result from GoldenModel : %h%h%h%h ", ctext[3], ctext[2], ctext[1], ctext[0]);	
 		$fdisplay (f,"Result from DUT : %h%h%h%h ", ds.cb.text_out[127:96], ds.cb.text_out[95:64], ds.cb.text_out[63:32], ds.cb.text_out[31:0]);
 
-		if (start == 1) begin
-			$fdisplay (g,"Encryption Number : %0d" , en_num);
-			$fdisplay (g,"KEY: %h%h%h%h", t.key[127:96], t.key[95:64], t.key[63:32], t.key[31:0]);
-			$fdisplay (g,"TEXT: %h%h%h%h", t.text[3], t.text[2], t.text[1], t.text[0]);
-			$fdisplay (g,"Result | GoldenModel : %h%h%h%h ", ctext[3], ctext[2], ctext[1], ctext[0]);	
-			$fdisplay (g,"Result | DUT : %h%h%h%h ", ds.cb.text_out[127:96], ds.cb.text_out[95:64], ds.cb.text_out[63:32], ds.cb.text_out[31:0]);
-			start = 0;
-		end	
+//		$fdisplay (f,"Result from GoldenModel : %h%h%h%h ", ctext[3], ctext[2], ctext[1], ctext[0]);	
+//		$fdisplay (f,"Result from DUT : %h%h%h%h ", ds.cb.text_out[127:96], ds.cb.text_out[95:64], ds.cb.text_out[63:32], ds.cb.text_out[31:0]);
+
 		
+	//	$display (" calling checker from test with status : %d  @ runtime %t ", t.status, $realtime); 
+
 		checker.check_result(ds.cb.text_out[31:0],  ds.cb.text_out[63:32], ds.cb.text_out[95:64],  
 				     ds.cb.text_out[127:96], ds.cb.done, ctext, t.done, t.status, rst_chk);
 

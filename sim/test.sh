87db7de53281326fe28e2eb89a76cb5df51612fc
Adil Sadik <sadik.adil@gmail.com>
2013-03-27 15:08:58 -0400

    added script

--- sim/test.sh
+++ sim/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-incr = "2"
-cat configure.txt | sed -e 's/INCR_MSB */INCR_MSB $incr /' > temp.txt
-rm -rf testfile.txt
-mv temp.txt testfile.txt
+INCR=0
+T=1
+for i in {0..10}
+do
+	cat configure.txt | sed -e "s/COPY_HERE/INCR_MSB $INCR/g" > temp1.txt
+	rm -rf configure.txt
+	mv temp1.txt configure.txt
+	make test 
+	cat configure.txt | sed -e "s/INCR_MSB $INCR/COPY_HERE/g" > temp2.txt
+	mv temp2.txt configure.txt
+	INCR=$(($T+$INCR))
+done

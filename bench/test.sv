e18d25f1db31a893f4b78444a8a5a38f094741e2
Adil Sadik <ams2378@columbia.edu>
2013-03-27 01:30:19 -0400

    edit

--- bench/test.sv
+++ bench/test.sv
@@ -66,7 +66,7 @@ program tb (ifc.bench ds);
 
 		t.randomize();
 
-		if (en_num == 10) begin
+		if (en_num == 11 && msbs <256) begin
 			msbs = msbs + 1;
 			en_num = 1;
 		end
@@ -256,6 +256,10 @@ program tb (ifc.bench ds);
 		$fdisplay (f,"Result from GoldenModel : %h%h%h%h ", ctext[3], ctext[2], ctext[1], ctext[0]);	
 		$fdisplay (f,"Result from DUT : %h%h%h%h ", ds.cb.text_out[127:96], ds.cb.text_out[95:64], ds.cb.text_out[63:32], ds.cb.text_out[31:0]);
 
+		if (ds.cb.done == 1) begin
+		$fdisplay (g,"Result GoldenModel : %h%h%h%h ", ctext[3], ctext[2], ctext[1], ctext[0]);	
+		$fdisplay (g,"Result DUT : %h%h%h%h ", ds.cb.text_out[127:96], ds.cb.text_out[95:64], ds.cb.text_out[63:32], ds.cb.text_out[31:0]);
+		end
 
 		
 	//	$display (" calling checker from test with status : %d  @ runtime %t ", t.status, $realtime); 
